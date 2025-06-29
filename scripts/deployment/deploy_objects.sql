d4d7ea9f3e27ac57261570636ed46ce999216d12
Abubakar Abdullahi <ajanifiles90@gmail.com>
2025-06-30 06:21:54 +0800

    - updated deploy path

--- scripts/deployment/deploy_objects.sql
+++ scripts/deployment/deploy_objects.sql
@@ -5,39 +5,39 @@ USE SCHEMA GIT_INTEGRATION;
 
 -- ==== STORES ==== 
 --- EXT SCHEMA
-execute immediate from '/scripts/snowflake_objects/stores/ext/create_STORES_STAGE.sql';
-execute immediate from '/scripts/snowflake_objects/stores/ext/create_STORES_EXT.sql';
-execute immediate from '/scripts/snowflake_objects/stores/ext/create_STORES_STREAM.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/stores/ext/create_STORES_STAGE.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/stores/ext/create_STORES_EXT.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/stores/ext/create_STORES_STREAM.sql';
 
 --- STG SCHEMA
-execute immediate from '/scripts/snowflake_objects/stores/stg/create_STORES_TBL_STG.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/stores/stg/create_STORES_TBL_STG.sql';
 
 --- DWH SCHEMA
 
 -- ==== TRANSACTIONS ==== 
 --- EXT SCHEMA
-execute immediate from '/scripts/snowflake_objects/transactions/ext/create_TRANSACTIONS_STAGE.sql';
-execute immediate from '/scripts/snowflake_objects/transactions/ext/create_TRANSACTIONS_EXT.sql';
-execute immediate from '/scripts/snowflake_objects/transactions/ext/create_TRANSACTIONS_STREAM.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/transactions/ext/create_TRANSACTIONS_STAGE.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/transactions/ext/create_TRANSACTIONS_EXT.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/transactions/ext/create_TRANSACTIONS_STREAM.sql';
 
 --- STG SCHEMA
-execute immediate from '/scripts/snowflake_objects/transactions/stg/create_TRANSACTIONS_TBL_STG.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/transactions/stg/create_TRANSACTIONS_TBL_STG.sql';
 
 --- DWH SCHEMA
-execute immediate from '/scripts/snowflake_objects/transactions/dwh/create_TRANSACTIONS_TBL.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/transactions/dwh/create_TRANSACTIONS_TBL.sql';
 
 --- RPT SCHEMA
 
 --- ========= ORCHESTRATION TASKS =============
-execute immediate from '/scripts/snowflake_objects/orchestration/pipeline_start_task.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/orchestration/pipeline_start_task.sql';
 -- stores
-execute immediate from '/scripts/snowflake_objects/orchestration/stores/create_EXTRACT_STORES_EXT.sql';
-execute immediate from '/scripts/snowflake_objects/orchestration/stores/create_INSERT_STORES_STG_TASK.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/orchestration/stores/create_EXTRACT_STORES_EXT.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/orchestration/stores/create_INSERT_STORES_STG_TASK.sql';
 
 -- transactions
-execute immediate from '/scripts/snowflake_objects/orchestration/transactions/create_EXTRACT_TRANSACTIONS_EXT.sql';
-execute immediate from '/scripts/snowflake_objects/orchestration/transactions/create_INSERT_TRANSACTIONS_STG_TASK.sql';
-execute immediate from '/scripts/snowflake_objects/orchestration/pipeline_end_task.sql'
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/orchestration/transactions/create_EXTRACT_TRANSACTIONS_EXT.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/orchestration/transactions/create_INSERT_TRANSACTIONS_STG_TASK.sql';
+execute immediate from '@GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT/branches/main/scripts/snowflake_objects/orchestration/pipeline_end_task.sql'
 
 
 -- snow sql -q "ALTER GIT REPOSITORY GFR_LOAD_DB.GIT_INTEGRATION.GFR_DWH_PROJECT FETCH"
